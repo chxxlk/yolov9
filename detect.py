d2280ac7fd21ed7321cd38088994d019e2790d27
chxxlk <chrislekpey06@gmail.com>
2026-04-11 12:24:26 +0700

    fix: detect.py

--- detect.py
+++ detect.py
@@ -105,7 +105,9 @@ def run(
         # pred = utils.general.apply_classifier(pred, classifier_model, im, im0s)
 
         # Process predictions
+        det = []
         for i, det in enumerate(pred):  # per image
+
             seen += 1
             if webcam:  # batch_size >= 1
                 p, im0, frame = path[i], im0s[i].copy(), dataset.count
